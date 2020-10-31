76fb850ce1eb3595d4164761c694a63f18e90c42
BloodFromPixels <blood.from.pixels@gmail.com>
2020-10-31 11:40:56 +0500

    Создан новый модуль addressbook-web-tests и создан первый тест с помощью Katalon Recorder

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,19 +3,14 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
-    hello("user");
-    hello("Aleksei");
+    Somebody m = new Somebody("world!");
+    System.out.println("Hello, " + m.someone());
 
     Square s = new Square(5);
     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(3, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-  }
-
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
   }
 
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Somebody.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+public class Somebody {
+
+  public String m;
+
+  public Somebody(String m) {
+    this.m = m;
+  }
+  public String someone() {
+    return this.m;
+  }
+}
+
