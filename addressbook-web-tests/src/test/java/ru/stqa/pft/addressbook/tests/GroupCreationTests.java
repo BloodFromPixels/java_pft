ddd72bd7265f2962b8d6e9d9032a73c1a3097a2c
BloodFromPixels <blood.from.pixels@gmail.com>
2020-12-05 16:26:12 +0500

    Настроена загрузка тестовых данных из файла для теста на создание групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -14,15 +15,23 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
+  // Провайдер тестовых данных
   @DataProvider
-  public Iterator<Object[]> validGroups() {
+  public Iterator<Object[]> validGroups() throws IOException {
     /*
     Заполнение списка массивов набором данных для одного запуска тестового метода (сколько наборов, столько и запусков)
     */
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-    list.add(new Object[] {new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-    list.add(new Object[] {new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+    // Чтение данных
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      // Делим полученные строки точкой с запятой
+      String[] split = line.split(";");
+      // Заполняем список полученными значениями
+      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     /*
     Возвращение итератора списка, который вытаскивает один набор параметров за другим, запускает тестовый метод
     несколько раз и помещает полученную информацию в отчёт
