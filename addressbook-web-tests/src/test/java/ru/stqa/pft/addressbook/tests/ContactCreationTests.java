01ce3d163c738f6d0ed1097ed23b9b3769fa0be0
BloodFromPixels <blood.from.pixels@gmail.com>
2020-12-05 18:02:16 +0500

    Реализован провайдер тестовых данных в тесте на создание контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,23 +1,47 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.SQLOutput;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase{
 
-  @Test
-  public void testContactCreation() {
+  // Провайдер тестовых данных с json
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validGroupsFromJson")
+  public void testContactCreation(ContactData contact) {
     Contacts before = app.contact().all();
     app.goTo().creationPage();
-    File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().withFirstname("test1").withLastname("test2").withPhoto(photo).withGroup("test1");
+    // File photo = new File("src/test/resources/stru.png");
     app.contact().create(contact, true);
     app.contact().goToHomePage();
     Contacts after = app.contact().all();
